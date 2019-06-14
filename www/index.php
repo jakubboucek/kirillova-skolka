277409d4c469082ed3c7f18792b6b9da7c9075f1
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2019-06-14 09:05:28 +0200

    Fix some bugs: repeated numbers in , welcome string. Add index checking

--- www/index.php
+++ www/index.php
@@ -2,21 +2,27 @@
 
 require './assets/nice-html.php';
 
-$id = $_GET['id'];
-
 $items = [
     1 => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     2 => "Pellentesque sapien. Praesent id justo in neque elementum ultrices.",
     3 => "Etiam egestas wisi a erat.",
     4 => "Pellentesque sapien.",
     5 => "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede.",
-    5 => "Nullam lectus justo, vulputate eget mollis sed, tempor sed magna.",
-    6 => "Aliquam erat volutpat. Praesent in mauris eu tortor porttitor accumsan.",
-    7 => "Etiam posuere lacus quis dolor.",
+    6 => "Nullam lectus justo, vulputate eget mollis sed, tempor sed magna.",
+    7 => "Aliquam erat volutpat. Praesent in mauris eu tortor porttitor accumsan.",
+    8 => "Etiam posuere lacus quis dolor.",
 ];
 
-if ($id > 0) {
+if (isset($_GET['id']) && $_GET['id'] > 0 && ($_GET['id']) <= count($items))
+{    
+    $id = $_GET['id'];
+    
     echo "Byla vybarána tato položka: " . $items[$id];
-} else {
-    echo "<form method=get>Zadejte číslo od 1 do 7: <input name=id><input type=submit>";
 }
+else
+{
+    echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
+}
+
+
+
