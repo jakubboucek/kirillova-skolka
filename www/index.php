c538fddec65fe61761480c3f538037a157aed631
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2019-06-14 12:37:23 +0200

    ! Split to two functions: form getting _GET('id') value and function for show form

--- www/index.php
+++ www/index.php
@@ -3,24 +3,41 @@
 require './assets/nice-html.php';
 
 $items = [
+    0 => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     1 => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     2 => "Pellentesque sapien. Praesent id justo in neque elementum ultrices.",
     3 => "Etiam egestas wisi a erat.",
     4 => "Pellentesque sapien.",
     5 => "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede.",
     6 => "Nullam lectus justo, vulputate eget mollis sed, tempor sed magna.",
-    7 => "Aliquam erat volutpat. Praesent in mauris eu tortor porttitor accumsan.",
+    11 => "Aliquam erat volutpat. Praesent in mauris eu tortor porttitor accumsan.",
     8 => "Etiam posuere lacus quis dolor.",
 ];
 
-if (isset($_GET['id']) && 
-    isset($items[$_GET['id']])) {    
-    $id = $_GET['id'];
-    
-    echo "Byla vybarána tato položka: " . $items[$id];
-} else {
-    echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
+function getId()
+{
+    if (isset($_GET['id'])) {
+        return $_GET['id'];
+    } else {
+        return null;
+    }
 }
+
+function getValueFromItems($items, $key) 
+{
+    if ($key!==null) {
+        if (isset($items[$key])) {
+            echo "Byla vybarána tato položka: " . $items[$key];
+        } else {
+            echo '<div>Error: Tato polozka neexistuje</div>';
+        }
+    } else {
+        echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
+    }
+}
+
+getValueFromItems($items, getId());
+
 
 
 
