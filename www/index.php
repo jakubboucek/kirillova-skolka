f18e5a4ad679892a229534616d9a48844df03c84
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2019-06-14 15:06:14 +0200

    ! Modify functions for catching strings without number

--- www/index.php
+++ www/index.php
@@ -1,10 +1,6 @@
 <?php
 declare(strict_types=1);
 
-
-
-echo file_get_contents('./assets/nice-html.html');
-
 $items = [
     0 => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     1 => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
@@ -17,49 +13,53 @@ $items = [
     8 => "Etiam posuere lacus quis dolor.",
 ];
 
-function getId(): ?int
-{
-    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-        return (int) $_GET['id'];
-    } 
-    
-    return null;
-}
+printValueFromItems($items, getId());
+
+
 
 function printValueFromItems($items, $key) 
 {
-    // if ($key!==null) {
-    //     if (isset($items[$key])) {
-    //         echo "Byla vybrána tato položka: " . $items[$key];
-    //     } else {
-    //         echo '<div>Error: Tato polozka neexistuje</div>';
-    //         http_response_code(404);
-    //     }
-    // } else {
-    //     http_response_code();
-    //     echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
-    // }  
-}
+    $item = getValueFromItems($items, $key);
+
+    if (http_response_code() != 404){
+        echo file_get_contents('./assets/nice-html.html');
+        if ($item != null) {
+            echo "Byla vybrána tato položka: " . $items[$key];
+        } else {
+            echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
+        }
+    }
+} 
 
 function getValueFromItems($items, $key)
 {
-    if ($key!==null) {
-        if (isset($items[$key])) {
-            return $items[$key];
-            // echo "Byla vybrána tato položka: " . $items[$key];
-        } else {
-            header(http_response_code(404));
-            // echo '<div>Error: Tato polozka neexistuje</div>';
-        }
-    } else {
-        header(http_response_code(404));
-        // http_response_code();
-        // echo '<form method="get">Zadejte číslo od 1 do 8: <input name="id"><input type="submit">';
+    if (http_response_code() != 404) {
+        if ($key!==null) {
+            if (isset($items[$key])) {
+                return $items[$key];
+            } else {
+                http_response_code(404);
+                return null;
+            }
+        } 
     }  
 }
 
+function getId(): ?int 
+{
+    if (isset($_GET['id'])) {
+        if (is_numeric($_GET['id'])) {
+            return (int) $_GET['id'];
+        }
+        else if ($_GET['id'] != ''){
+            http_response_code(404);
+            return null;
+        }
+    }
 
-printValueFromItems($items, getId());
+    return null;
+}
+
 
 
 
